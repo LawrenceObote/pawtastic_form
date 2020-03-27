fa82718fb697a906abd51f52b831ad64c489f06d
Lawrence Obote <dev.obote@gmail.com>
2020-03-27 15:52:48 -0400

    Added more to form

--- pawtastic/src/App.js
+++ pawtastic/src/App.js
@@ -15,7 +15,26 @@ export default class App extends Component{
         <div className="right">
           <h1>Yay, we love dogs! Give us the basics about your pup.</h1>
           <form>
-              <input type="text" value={this.state.value}
+            <div>
+              <h3>Name</h3>
+              <input type="text" placeholder="Dog's name" value={this.state.value}></input>
+              <button type="upload" placeholder="upload a photo"></button>
+              <h3>Upload a photo</h3>
+            </div>
+            <div>
+              <h3>Breed</h3>
+              <input type="text" placeholder=""></input>
+              <h3>Birthday</h3>
+              <input type="text" placeholder="MM/DD/YYYY"></input>
+            </div>
+            <div>
+              <h3>Gender</h3>
+              <input type="radio" id="male" name="gender" value="male" className="radio"></input>
+              <label for="toggle" class="switch" className="switch"></label>
+              
+              <input type="radio" id="female" name="gender" value="female" className="radio"></input>
+              <label for="toggle" class="switch" className="switch"></label>
+            </div>
           </form>
         </div>
       </div>
@@ -23,4 +42,3 @@ export default class App extends Component{
   }
 }
 
-export default App;
