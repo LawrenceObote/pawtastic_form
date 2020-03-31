1b9738127b0daea901f23a436a04dbaa78fa8ee8
Lawrence Obote <dev.obote@gmail.com>
2020-03-30 21:23:45 -0400

    Added some css colors

--- pawtastic/src/App.js
+++ pawtastic/src/App.js
@@ -15,31 +15,59 @@ export default class App extends Component{
         <div className="right">
           <h1>Yay, we love dogs! Give us the basics about your pup.</h1>
           <form>
-            <div>
-              <h3>Name</h3>
-              <input type="text" placeholder="Dog's name" value={this.state.value}></input>
+            <div className="textBars">
+              <div className="col-25">
+              <label for="name">Name</label>
+              </div>
+              <div className="col-75">
+              <input type="text" placeholder="Dog's name" id="name" value={this.state.value}></input>
+              </div>
+              
+              <div className="col-25" id="photo">
               <button type="upload" placeholder="upload a photo"></button>
-              <h3>Upload a photo</h3>
+              </div>
+              <div className="col-75">
+              <p>Upload a photo</p>
+              </div>
             </div>
-            <div>
-              <h3>Breed</h3>
-              <input type="text" placeholder=""></input>
-              <h3>Birthday</h3>
+            <div className="textBars">
+              <div class="col-25">
+              <label for="breed">Breed</label>
+              </div>
+              <div class="col-75">
+              <input type="text" placeholder="" id="breed"></input>
+              </div>
+              
+              <div class="col-25">
+              <label for="birthday">Birthday</label>
+              </div>
+              <div class="col-75">
               <input type="text" placeholder="MM/DD/YYYY"></input>
-            </div>
-            <div>
-              <h3>Gender</h3>
+              </div>
+              </div>
+            
+            
+            <div className="buttons">
+              <label for="gender" id="gender">Gender</label>
+              <div className="input">
               <input type="checkbox" id="male" name="gender" value="male" className="checkbox"></input>
               <label for="toggle" class="switch" className="switch"></label>
-              
+              </div>
+
+              <div className="input">
               <input type="checkbox" id="female" name="gender" value="female" className="checkbox"></input>
               <label for="toggle" class="switch" className="switch"></label>
-
+              </div>
+              <label for="spayedOrNeutered">Spayed or Neutered</label>
+              <div className="input">
               <input type="radio" id="yes" name="spayedOrNeutered" value="yes" className="radio"></input>
               <label for="toggle" class="switch" className="switch"></label>
-              
+              </div>
+
+              <div className="input">
               <input type="radio" id="no" name="spayedOrNeutered" value="no" className="radio"></input>
               <label for="toggle" class="switch" className="switch"></label>
+              </div>
             </div>
           </form>
         </div>
